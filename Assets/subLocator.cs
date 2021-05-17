488e22ac5de78f83043a4978b264f6f94e08e2de
Chris Congleton <70895890+Chris-Congleton@users.noreply.github.com>
2021-05-17 10:41:43 +0200

    Drag based on height map
    The ship locator object reads the grey scale of the canvas which corresponds to the height map. This greyscale value is used to set the drag of the ship thus affecting the ship speed.

--- Assets/subLocator.cs
+++ Assets/subLocator.cs
@@ -9,6 +9,8 @@ public class subLocator : MonoBehaviour
     [SerializeField] RawImage image;
     [SerializeField] Camera cam;
 
+    public float gs;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,20 +28,12 @@ public class subLocator : MonoBehaviour
         int x = (int) screenPos.x;
         int y = (int) screenPos.y;
 
-        RectTransform rt = image.rectTransform;
-        float width = rt.rect.width;
-        float height = rt.rect.height;
-
         Texture2D tex2d = (Texture2D)image.texture;
-
         Color c = tex2d.GetPixel(x,y);
-        float gs = c.b;
+        // Calculate greyscale
+        // gs = (R + G + B) / 3
+        gs = (c.r + c.g + c.b)/3;
 
-        Debug.Log("x is " + x);
-        Debug.Log("y is " + y);
-        Debug.Log("gs is " + gs);
-        
-        //Debug.Log("Value is " + mapValue);
-        //Debug.Log()
+        //Debug.Log("gs is " + gs);
     }
 }
